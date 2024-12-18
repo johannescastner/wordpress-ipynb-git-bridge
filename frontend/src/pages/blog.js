c4fdcb08468d86d2979babcccc6ffd25fc01a260
Johannes Castner <j.castner@kingston.ac.uk>
2024-12-18 15:25:58 +0000

    Initial commit - backend setup with FastAPI and static files

--- frontend/src/pages/blog.js
+++ frontend/src/pages/blog.js
@@ -1,1 +1,26 @@
- 
+import { useState, useEffect } from "react";
+
+export default function Blog() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blog")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Blog</h1>
+      <ul>
+        {blogs.map((blog) => (
+          <li key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>{blog.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,1 +1,9 @@
- 
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Towards People</h1>
+      <p>Enhancing collaboration between humans and AI.</p>
+    </div>
+  );
+}
+
